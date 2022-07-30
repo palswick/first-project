8de24b522b09e6ff0d762b4268e7726835466c4c
Mikhail Sushchikh <sumux200013@gmail.com>
2022-07-31 00:03:12 +0400

    42nd lesson (Redux)

---
+++ src/redux/redux-store.js
@@ -0,0 +1,16 @@
+import {combineReducers, createStore} from "redux";
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
+let reducers = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebarFriendsList: sidebarReducer,
+});
+
+
+let store = createStore(reducers);
+
+
+export default store;
